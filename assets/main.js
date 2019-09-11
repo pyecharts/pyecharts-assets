16f2026763d7cab40556eb279a383a06df4531a7
chenjiandongx <chenjiandongx@qq.com>
2019-09-11 23:11:36 +0800

    Add: main.js

---
+++ assets/main.js
@@ -0,0 +1,9 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var version = '1.4.0';
+    function load_ipython_extension() {
+        console.log("pyecharts-assert extension has been loaded");
+    }
+    exports.load_ipython_extension = load_ipython_extension;
+});
