fd725afb9364c096ce40c7f7e64c0158f92343c7
sunhailinLeo <379978424@qq.com>
2021-06-10 11:51:43 +0800

    update v5 folder

---
+++ assets/v5/main.js
@@ -0,0 +1,8 @@
+define(["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function load_ipython_extension() {
+        console.log("pyecharts-assert extension has been loaded");
+    }
+    exports.load_ipython_extension = load_ipython_extension;
+});
